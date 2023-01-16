65cce06ac694f07639a254ffd9406cafc449bee1
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-01-16 19:42:42 +0200

    Add project files.

---
+++ asp_net_mvc_pv125/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using asp_net_mvc_pv125.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace asp_net_mvc_pv125.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // code...
+
+            // View search pattern: ~/Views/{ControllerName}/{ActionName}
+            // Current view path: ~Views/Home/Index.cshtml
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
