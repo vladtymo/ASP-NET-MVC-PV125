6265e23459a04e6532e95cfa3f79f85476d62c01
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-01-18 19:09:00 +0200

    Put model to view. Render array of the objects.

---
+++ asp_net_mvc_pv125/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using asp_net_mvc_pv125.Helpers;
+using asp_net_mvc_pv125.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_pv125.Controllers
+{
+    public class ProductsController : Controller
+    {
+        List<Product> products = new List<Product>(Seeder.GetProducts());
+        public ProductsController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            // TODO: get data from db
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            // get product by id
+            var product = products.FirstOrDefault(x => x.Id == id);
+
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+    }
+}
---
+++ asp_net_mvc_pv125/Helpers/Seeder.cs
@@ -0,0 +1,49 @@
+﻿using asp_net_mvc_pv125.Models;
+
+namespace asp_net_mvc_pv125.Helpers
+{
+    public static class Seeder
+    {
+        public static IEnumerable<Product> GetProducts()
+        {
+            return new Product[]
+            {
+                new Product() 
+                {
+                    Id = 1,
+                    Name = "Google Pixel 7 Pro",
+                    Price = 1200,
+                    Category = "Electronics"
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "iPhone 14 Pro",
+                    Price = 999,
+                    Category = "Electronics"
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Name = "T-Shirt",
+                    Price = 60,
+                    Category = "Clothes"
+                },
+                new Product()
+                {
+                    Id = 4,
+                    Name = "Xiaomi Powerbank",
+                    Price = 120,
+                    Category = "Accesories"
+                },
+                new Product()
+                {
+                    Id = 5,
+                    Name = "Apple HomePod Mini",
+                    Price = 450,
+                    Category = "Home Electronics"
+                }
+            };
+        }
+    }
+}
